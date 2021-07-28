e4eb8256beb07b256fce69958216a8240d3afb45
hadarmarom <hadarmarom1@gmail.com>
2021-07-28 11:53:47 +0300

    hadar ChatUpApp created

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import  ChatUpApp  from './pages/ChatUpApp.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hadar is in the houseeee</Text>
-      <Text>Hadar is in the houseeee</Text>
+      <ChatUpApp/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ pages/ChatUpApp.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+import React from 'react'
+
+const ChatUpApp = () => {
+
+    return (
+        <View>
+            <Text>chatUpApp</Text>
+        </View>
+    )
+}
+export default ChatUpApp
